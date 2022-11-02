61fd37ece2804d5d7972f56dd01a34f5d2ae0962
Wheel <1462965949@qq.com>
2022-11-02 14:07:28 +0800

    Update voxelizer.h

--- voxelizer.h
+++ voxelizer.h
@@ -8,11 +8,28 @@
 #include <stdbool.h>
 
 
+#define _VL_IN_
+#define _VL_OUT_
+#define _VL_OPT_IN_
+#define _VL_OPT_OUT_
+
+
 #define VL_MIN(a, b) ((a) > (b) ? (b) : (a))
 #define VL_MAX(a, b) ((a) < (b) ? (b) : (a))
 
 
-typedef struct { double x, y, z; } VL_Vector3;
+#define VL_HIGHP
+#ifdef VL_HIGHP
+typedef double VL_Float;
+typedef size_t VL_Size;
+#else
+typedef float VL_Float;
+typedef unsigned int VL_Size;
+#endif
+
+
+
+typedef struct { VL_Float x, y, z; } VL_Vector3F;
 
 
 /*
@@ -20,12 +37,57 @@ typedef struct { double x, y, z; } VL_Vector3;
  * Be easy when using add, sub, mul and div, I've added temp variable to avoid cyclic operation
  * eg: add(&v1, &v1, &v2);
  */
-extern void vl_vec3_add(VL_Vector3 * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
-extern void vl_vec3_sub(VL_Vector3 * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
-extern void vl_vec3_mul(VL_Vector3 * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
-extern void vl_vec3_div(VL_Vector3 * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
-extern void vl_vec3_dot(double * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
-extern void vl_vec3_cross(VL_Vector3 * out, const VL_Vector3 * const a, const VL_Vector3 * const b);
+extern void vl_vec3_add(VL_Vector3F * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_sub(VL_Vector3F * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_mul(VL_Vector3F * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_div(VL_Vector3F * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_dist(VL_Float * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_dot(VL_Float * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+extern void vl_vec3_cross(VL_Vector3F * out, const VL_Vector3F * const a, const VL_Vector3F * const b);
+
+
+/*
+ * Get point cloud difinition from mesh in pre calculation
+ *
+ * @outcx        Output definition in x axis
+ * @outcy        Output definition in y axis
+ * @outcz        Output definition in z axis
+ * @outvmin:     Output bbox min
+ * @outvmax:     Output bbox max
+ * @inverts:     Input verts
+ * @innverts:    Input vert count
+ * @invsize:     Input voxel size
+ */
+extern void vl_point_cloud_res_from_mesh(
+	_VL_OPT_OUT_ VL_Size *                 out_cx,
+	_VL_OPT_OUT_ VL_Size *                 out_cy,
+	_VL_OPT_OUT_ VL_Size *                 out_cz,
+	_VL_OPT_OUT_ VL_Vector3F * const       out_vmin,
+	_VL_OPT_OUT_ VL_Vector3F * const       out_vmax,
+	_VL_IN_      const VL_Vector3F * const in_verts,
+	_VL_IN_      const VL_Size             in_nverts,
+	_VL_IN_      const VL_Float            in_vsize
+	);
+
+
+/*
+ * Get point cloud difinition from bbox in pre calculation
+ *
+ * @outcx        Output definition in x axis
+ * @outcy        Output definition in y axis
+ * @outcz        Output definition in z axis
+ * @invmin:      Input bbox min
+ * @invmax:      Input bbox max
+ * @invsize:     Input voxel size
+ */
+extern void vl_point_cloud_res_from_bbox(
+	_VL_OUT_ VL_Size *                 out_cx,
+	_VL_OUT_ VL_Size *                 out_cy,
+	_VL_OUT_ VL_Size *                 out_cz,
+	_VL_IN_  const VL_Vector3F * const in_vmin,
+	_VL_IN_  const VL_Vector3F * const in_vmax,
+	_VL_IN_  const VL_Float invsize
+	);
 
 
 /*
@@ -40,14 +102,14 @@ extern void vl_vec3_cross(VL_Vector3 * out, const VL_Vector3 * const a, const VL
  * @nfaces:      Input face count
  * @vsize:       Input voxel size
  */
-extern VL_Vector3 * vl_point_cloud_from_mesh(
-	VL_Vector3 ** const point_cloud,
-	size_t * npoints,
-	const VL_Vector3 * const verts,
-	const size_t nverts,
-	const size_t * const faces,
-	const size_t nfaces,
-	const double vsize
+extern VL_Vector3F * vl_point_cloud_from_mesh(
+	_VL_OPT_OUT_ VL_Vector3F ** const      out_point_cloud,
+	_VL_OUT_     VL_Size * const           out_npoints,
+	_VL_IN_      const VL_Vector3F * const in_verts,
+	_VL_IN_      const VL_Size             in_nverts,
+	_VL_IN_      const VL_Size * const     in_faces,
+	_VL_IN_      const VL_Size             in_nfaces,
+	_VL_IN_      const VL_Float            in_vsize
 	);
 
 
@@ -63,13 +125,13 @@ extern VL_Vector3 * vl_point_cloud_from_mesh(
  * @vsize:       Input voxel size
  */
 extern void vl_mesh_from_point_cloud(
-	VL_Vector3 ** const verts,
-	size_t * const nverts,
-	size_t ** const faces,
-	size_t * const nfaces,
-	const VL_Vector3 * const point_cloud,
-	size_t npoints,
-	const double vsize
+	_VL_OUT_ VL_Vector3F ** const      out_verts,
+	_VL_OUT_ VL_Size * const           out_nverts,
+	_VL_OUT_ VL_Size ** const          out_faces,
+	_VL_OUT_ VL_Size * const           out_nfaces,
+	_VL_IN_  const VL_Vector3F * const in_point_cloud,
+	_VL_IN_  VL_Size                   in_npoints,
+	_VL_IN_  const VL_Float            in_vsize
 	);
 
 
@@ -84,14 +146,13 @@ extern void vl_mesh_from_point_cloud(
  * @nfaces:      Input face count
  * @vsize:       Input voxel size
  */
-extern double vl_volume_from_mesh(
-	const VL_Vector3 * verts,
-	const size_t nverts,
-	const size_t * faces,
-	const size_t nfaces,
-	const double vsize
+extern VL_Float vl_volume_from_mesh(
+	_VL_IN_ const VL_Vector3F * in_verts,
+	_VL_IN_ const VL_Size       in_nverts,
+	_VL_IN_ const VL_Size *     in_faces,
+	_VL_IN_ const VL_Size       in_nfaces,
+	_VL_IN_ const VL_Float      in_vsize
 	);
 
 
 #endif
-
